25bf4829ba15ff8a64526a79a14642edf0157652
evilMorty <michele.xiloyannis@akina.health>
2022-10-09 19:02:41 +0200

    Added styling, tailwind not needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { DOMMessage, DOMMessageResponse } from './types';
  
 function App() {
+
+  const [title, setTitle] = useState('');
+  const [headlines, setHeadlines] = useState<string[]>([]);
+
+ useEffect(() => {
+    chrome.tabs && chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, tabs => {
+      chrome.tabs.sendMessage(
+        tabs[0].id || 0,
+        { type: 'GET_DOM' } as DOMMessage,
+        (response: DOMMessageResponse) => {
+          setTitle(response.title);
+          setHeadlines(response.headlines);
+        });
+    });
+  });
+
  return (
-   <div className="App">
-     <h1>SEO Extension built with React!</h1>
- 
-     <ul className="SEOForm ">
-       <li className="SEOValidation ">
-         <div className="SEOValidationField ">
-           <span className="SEOValidationFieldTitle ">Title</span>
-           <span className="SEOValidationFieldStatus Error bg-red-600">
-             90 Characters
-           </span>
-         </div>
-         <div className="SEOVAlidationFieldValue">
-           The title of the page
-         </div>
-       </li>
- 
-       <li className="SEOValidation">
-         <div className="SEOValidationField">
-           <span className="SEOValidationFieldTitle">Main Heading</span>
-           <span className="SEOValidationFieldStatus Ok">
-             1
-           </span>
-         </div>
-         <div className="SEOVAlidationFieldValue">
-           The main headline of the page (H1)
-         </div>
-       </li>
-     </ul>
-   </div>
- );
+  <div className="App">
+  <h1>SEO Extension built with React!</h1>
+
+  <ul className="SEOForm">
+    <li className="SEOValidation">
+      <div className="SEOValidationField">
+        <span className="SEOValidationFieldTitle">Title</span>
+        <span className={`SEOValidationFieldStatus ${title.length < 30 || title.length > 65 ? 'Error' : 'Ok'}`}>
+          {title.length} Characters
+        </span>
+      </div>
+      <div className="SEOVAlidationFieldValue">
+        {title}
+      </div>
+    </li>
+
+    <li className="SEOValidation">
+      <div className="SEOValidationField">
+        <span className="SEOValidationFieldTitle">Main Heading</span>
+        <span className={`SEOValidationFieldStatus ${headlines.length !== 1 ? 'Error' : 'Ok'}`}>
+          {headlines.length}
+        </span>
+      </div>
+      <div className="SEOVAlidationFieldValue">
+        <ul>
+          {headlines.map((headline, index) => (<li key={index}>{headline}</li>))}
+        </ul>
+      </div>
+    </li>
+  </ul>
+</div>
+);
 }
  
 export default App;
